73d83c7de0eb6ccee3ffed226da834675a19eee6
choccho <shocho0101@hotmail.co.jp>
2020-02-16 10:37:31 +0900

    use xib

--- Reiwashi/AppDelegate.swift
+++ Reiwashi/AppDelegate.swift
@@ -11,10 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController =  UINavigationController(rootViewController: HistoryViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Reiwashi/HistoryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HistoryViewController.swift
+//  Reiwashi
+//
+//  Created by 張翔 on 2020/02/16.
+//  Copyright © 2020 2222_d. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "HistoryViewController", bundle: Bundle(for: type(of: self)))
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
